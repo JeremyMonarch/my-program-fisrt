ae72c2c6980b8b7987f497b2cb205c29026145aa
Mykyta Zakharov <mykytazakh@gmail.com>
2022-05-07 19:31:47 +0300

    Revert "Added my homework (Methods)"
    This reverts commit 862e41f33d893581f2a42c994fd633e2561ef70c.

--- Lesson04.Methods/Lesson04.Methods/Program.cs
+++ Lesson04.Methods/Lesson04.Methods/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace Lesson04.Methods
 {
@@ -35,8 +34,6 @@ namespace Lesson04.Methods
             Concat("10", "20", "30", "40", "50");
 
             Console.WriteLine(Factorial(5));
-
-            Master();
         }
 
         static int FactorialRecursive(int value)
@@ -118,65 +115,6 @@ namespace Lesson04.Methods
             }
 
             return sum;
-        }
-
-        //-------------------------------------------HOMEWORK------------------------------------------//
-
-        static void Master()
-        {
-            Console.Write("Input first number: ");
-            int a = int.Parse(Console.ReadLine());
-            Console.Write("Input second number: ");
-            int b = int.Parse(Console.ReadLine());
-
-            Console.Write("Input string: ");
-            string x = Console.ReadLine();
-
-            MaxMinValue(54, 76);
-            MaxMinValue(3, 43, 23);
-            MaxMinValue(4.3, 9.23, 1.11, 13.32);
-
-            Repeat(x);
-
-            if (TrySumIfOdd(a, b, out int sum))
-            {
-                Console.WriteLine("\nParity \nSum is: " + sum);
-            }
-
-            else
-            {
-                Console.WriteLine("\nNot Parity \nSum is: " + sum);
-            }
-
-        }
-        static void MaxMinValue(int x, int y)
-        {
-            Console.WriteLine("Max value is: " + Math.Max(x, y));
-            Console.WriteLine("Min value is: " + Math.Min(x, y));
-        }
-
-        static void MaxMinValue(int x, int y, int z)
-        {
-            Console.WriteLine("\nMax value is: " + Math.Max(x, Math.Max(y, z)));
-            Console.WriteLine("Min value is: " + Math.Min(x, Math.Min(y, z)));
-        }
-
-        static void MaxMinValue(double x, double y, double z, double w)
-        {
-            Console.WriteLine("\nMax value is: " + Math.Max(x, Math.Max(y, Math.Max(z, w))));
-            Console.WriteLine("Min value is: " + Math.Min(x, Math.Min(y, Math.Min(z, w))));
-        }
-
-        static bool TrySumIfOdd(int a, int b, out int sum)
-        {
-            sum = a + b;
-            return sum % 2 == 0;
-        }
-
-        static void Repeat(string x)
-        {
-            x = String.Concat(Enumerable.Repeat(x, 3));
-            Console.WriteLine("Your triple string: " + x);
         }
     }
 }
