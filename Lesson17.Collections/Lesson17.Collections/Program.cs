d69250db1e6adc40a62b9a6269a069d06a3f229f
Mykyta Zakharov <mykytazakh@gmail.com>
2022-06-08 19:06:38 +0300

    Revert "Revert "add classwork""
    This reverts commit 920650629a40c0ea1ebfee90c7b90d346bf2a479.

---
+++ Lesson17.Collections/Lesson17.Collections/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lesson17.Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new LinkedList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+
+            foreach (var item in list.GetAll())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(list.Contains(1));
+
+            var stringList = new LinkedList<string>
+            {
+               "sewfs",
+               "asfawdaw222"
+            };
+
+            foreach (var item in stringList.GetAll())
+            {
+                Console.WriteLine(item);
+            }
+
+            var set = new Set<int>(100);
+            set.Add(1);
+            set.Add(2);
+            set.Add(3);
+            set.Add(33);
+
+            Console.WriteLine(set.Contains(1));
+            Console.WriteLine(set.Contains(3));
+            Console.WriteLine(set.Contains(33));
+            Console.WriteLine(set.Contains(45));
+
+
+        }
+    }
+}
